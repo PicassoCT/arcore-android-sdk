4c63cbe6e697831d16b0ad013bde83ae27f11c1e
PicassoCT <PicassoCT@gmx.de>
2018-07-04 00:29:50 +0200

    Hologram Example

--- arcore-android-sdk/samples/hello_ar/app/src/main/java/com/google/ar/core/examples/app/common/rendering/SpringOverlayRenderer.java
+++ arcore-android-sdk/samples/hello_ar/app/src/main/java/com/google/ar/core/examples/app/common/rendering/SpringOverlayRenderer.java
@@ -284,7 +284,8 @@ public class SpringOverlayRenderer implements IPackageRecivedCallback {
         GLES20.glUseProgram(overlayProgram);
 
         GLES20.glEnable(GLES20.GL_BLEND);
-        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA,GLES20.GL_ONE_MINUS_SRC_ALPHA);
+        //GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA,GLES20.GL_ONE_MINUS_SRC_ALPHA);
+        GLES20.glBlendFunc(GLES20.GL_SRC_ALPHA,GLES20.GL_ONE_MINUS_CONSTANT_COLOR);
         GLES20.glBlendEquation(GLES20.GL_FUNC_ADD);
 
 
